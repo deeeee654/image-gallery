f34e529d16ae1c13aa7d4eec7fc4c3331f418478
deeeee654 <ebele7630@gmail.com>
2026-01-16 13:05:56 +0000

    update2

--- gallery.js
+++ gallery.js
@@ -1,1 +1,34 @@
 console.log("This is an image gallery.");
+
+const backing = document.querySelector(".pics");
+
+const images = [
+  {
+    src: "img/streetwear/chic.jpg",
+    alt: "streetwear picture",
+  },
+  {
+    src: "img/y2k/y2k.jpg",
+    alt: "2000s",
+  },
+  {
+    src: "img/alt/aali.jpg",
+    alt: "alternative",
+  },
+  {
+    src: "img/coquette/coq1.webp",
+    alt: "coquette girl",
+  },
+];
+
+function createFront() {
+  images.forEach(function (image) {
+    const imgElement = document.createElement("img");
+
+    imgElement.src = image.src;
+    imgElement.alt = image.alt;
+
+    backing.append(imgElement);
+  });
+}
+createFront();
